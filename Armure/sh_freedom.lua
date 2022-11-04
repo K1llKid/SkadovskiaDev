3db655645cbe91d817a3fd75bb230e8af0a97993
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2022-11-05 00:38:27 +0100

    Manteau Freedom
     Manteau de cuir au couleur de la faction.

---
+++ Armure/sh_freedom.lua
@@ -0,0 +1,104 @@
+ITEM.name = "Manteau FREEDOM"
+ITEM.model ="models/kek1ch/scientific_outfit.mdl"
+ITEM.newModel ="models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl"
+ITEM.description= "Ancien Manteau Freedom"
+ITEM.longdesc = "Un manteau conçu par la faction des Freedom, c'est un manteau de cuir au couleur de la faction."
+ITEM.width = 2
+ITEM.height = 2
+ITEM.price = 10000
+ITEM.busflag = "dev"
+ITEM.br = 0
+ITEM.fbr = 0 
+ITEM.ar = 0
+ITEM.far = 0
+ITEM.isHelmet = true
+ITEM.isGasmask = false
+ITEM.radProt = 0.9
+ITEM.repairCost = ITEM.price/100*1
+ITEM.ballisticlevels = {"0","l","l","l","l"}
+ITEM.img = ix.util.GetMaterial("cotz/ui/icons/outfit_jacket_freedom1.png")
+ITEM.noBusiness = true
+
+ITEM.weight = 0.700
+ITEM.miscslots = 1
+
+ITEM.skincustom[1] = {
+	name = "Loner",
+	skingroup = 0,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[2] = {
+	name = "Loner 2",
+	skingroup = 1,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[3] = {
+	name = "Loner 3",
+	skingroup = 2, 
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[4] = {
+	name = "Duty",
+	skingroup = 0,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[5] = {
+	name = "Duty 2",
+	skingroup = 1,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[6] = {
+	name = "Duty 3",
+	skingroup = 2,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[7] = {
+	name = "Merc",
+	skingroup = 0,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[8] = {
+	name = "Merc 2",
+	skingroup = 1,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[9] = {
+	name = "Merc 3",
+	skingroup = 2,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[10] = {
+	name = "Freedom",
+	skingroup = 0,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[11] = {
+	name = "Freedom 2",
+	skingroup = 1,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[12] = {
+	name = "Freedom 3",
+	skingroup = 2,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[13] = {
+	name = "Ecologist",
+	skingroup = 0,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[14] = {
+	name = "Monolith",
+	skingroup = 0,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[15] = {
+	name = "Monolith 2",
+	skingroup = 1,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
+ITEM.skincustom[16] = {
+	name = "Monolith 3",
+	skingroup = 2,
+	modelOverride = "models/npc/stalker_freedom/freedom_jacket/freedom_jacket.mdl",
+}
