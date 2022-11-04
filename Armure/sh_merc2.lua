88fe2a2d347be35349557e4932b76d74f1e7127c
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2022-11-05 00:09:38 +0100

    Armure MKII
    L'armure des Mercenaires MKII permet une bien meilleur protection, elle possède 3 slots d'attachements pratique pour des plaques pare balle.

---
+++ Armure/sh_merc2.lua
@@ -0,0 +1,80 @@
+ITEM.name = "Armure Mercenaire MKII" 
+ITEM.model = "models/lostsignalproject/items/upgrades/upgro3.mdl"
+ITEM.newModel = "models/stalker_merc/stalker_merc2b.mdl"
+ITEM.description= "Une armure mercenaire de niveau 2" 
+ITEM.longdesc = "Cette armure de mercenaire est une plustôt bonne armure car elle protège bien mieux que se précédante et elle montre ainsi vôtre professionalisme au sein de la faction."
+ITEM.width = 2
+ITEM.height = 3
+ITEM.price = 60000
+ITEM.busflag = "dev"
+ITEM.br = 0.40
+ITEM.fbr = 9
+ITEM.ar = 0.15
+ITEM.far = 2
+ITEM.isHelmet = true
+ITEM.repairCost = ITEM.price/100*1
+ITEM.ballisticlevels = {"ll","lll","lll","lll","l"}
+ITEM.img = ix.util.GetMaterial("cotz/ui/icons/outfit_35.png")
+ITEM.noBusiness = true
+
+ITEM.weight = 8.800
+ITEM.miscslots = 3
+
+ITEM.newSkin = 0
+ITEM.bodygroup = {0}
+ITEM.bodygroupsub = {0}
+
+
+
+ITEM.skincustom[1] = {
+	name = "Skin 0",
+	skingroup = 0,
+}
+ITEM.skincustom[2] = {
+	name = "Skin 1",
+	skingroup = 1,
+}
+ITEM.skincustom[3] = {
+	name = "Skin 2",
+	skingroup = 2,
+}
+ITEM.skincustom[4] = {
+	name = "Skin 3",
+	skingroup = 3,
+}
+ITEM.skincustom[5] = {
+	name = "Skin 4",
+	skingroup = 4,
+}
+ITEM.skincustom[6] = {
+	name = "Skin 5",
+	skingroup = 5,
+}
+ITEM.skincustom[7] = {
+	name = "Skin 6",
+	skingroup = 6,
+}
+ITEM.skincustom[8] = {
+	name = "Skin 7",
+	skingroup = 7,
+}
+ITEM.skincustom[9] = {
+	name = "Skin 8",
+	skingroup = 8,
+}
+ITEM.skincustom[10] = {
+	name = "Skin 9",
+	skingroup = 9,
+}
+ITEM.skincustom[11] = {
+	name = "Skin 10",
+	skingroup = 10,
+}
+ITEM.skincustom[12] = {
+	name = "Skin 11",
+	skingroup = 11,
+}
+ITEM.skincustom[13] = {
+	name = "Skin 12",
+	skingroup = 12,
+}
