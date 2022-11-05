2222dabb7ed76749fe0eb918a5bc85b9720fe7c6
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2022-11-05 01:13:46 +0100

    Create sh_plugin.lua

---
+++ plugin/utilitymenu/sh_plugin.lua
@@ -0,0 +1,8 @@
+local PLUGIN = PLUGIN
+
+local PLUGIN = PLUGIN
+PLUGIN.name = "Utility Menu"
+PLUGIN.author = "some faggot"
+PLUGIN.desc = "Un menu qui permet d'afficher certaines informations par la touche F1"
+
+ix.util.Include("sv_plugin.lua")
