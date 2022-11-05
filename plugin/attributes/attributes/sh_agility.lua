aa3d2dbcbf1cea4ade75b400e3b0ae26f674f61a
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2022-11-05 14:36:30 +0100

    Create sh_agility.lua

---
+++ plugin/attributes/attributes/sh_agility.lua
@@ -0,0 +1,3 @@
+ATTRIBUTE.name = "Agilité (AGI)"
+ATTRIBUTE.description = "Finesse, rapidité, réflexes. Détermine la vitesse de déplacement, les prouesses d'acrobatie et d'agilité. En combat, l'agilité la plus élevée peut agir en premier."
+ATTRIBUTE.shortname = "AGI"
