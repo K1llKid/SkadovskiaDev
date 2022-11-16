ccfe317445e00988e3957a2c95b1e28dfe5596b5
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2022-11-16 21:14:35 +0100

    Armure Bandit #1
    C'est un manteau basique destiné aux bandits !

---
+++ Armure/sh_bandit.lua
@@ -0,0 +1,104 @@
+ITEM.name = "Manteau Bandit"
+ITEM.model ="models/lostsignalproject/items/upgrades/upgro2.mdl"
+ITEM.newModel ="models/stalker_greh/stalker_greh_1a_face_01.mdl"
+ITEM.description= "Ancien Manteau Freedom"
+ITEM.longdesc = "Un manteau conçu par la faction des Freedom, c'est un manteau de cuir au couleur de la faction."
+ITEM.width = 2
+ITEM.height = 2
+ITEM.price = 10000
+ITEM.busflag = "dev"
+ITEM.br = 0
+ITEM.fbr = 0 
+ITEM.ar = 0
+ITEM.far = 0
+ITEM.isHelmet = true
+ITEM.isGasmask = false
+ITEM.radProt = 0.9
+ITEM.repairCost = ITEM.price/100*1
+ITEM.ballisticlevels = {"0","l","l","l","l"}
+ITEM.img = ix.util.GetMaterial("cotz/ui/icons/outfit_jacket_duty2.png")
+ITEM.noBusiness = true
+
+ITEM.weight = 0.700
+ITEM.miscslots = 1
+
+ITEM.skincustom[1] = {
+	name = "Loner",
+	skingroup = 0,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[2] = {
+	name = "Loner 2",
+	skingroup = 1,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[3] = {
+	name = "Loner 3",
+	skingroup = 2, 
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[4] = {
+	name = "Duty",
+	skingroup = 0,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[5] = {
+	name = "Duty 2",
+	skingroup = 1,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[6] = {
+	name = "Duty 3",
+	skingroup = 2,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[7] = {
+	name = "Merc",
+	skingroup = 0,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[8] = {
+	name = "Merc 2",
+	skingroup = 1,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[9] = {
+	name = "Merc 3",
+	skingroup = 2,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[10] = {
+	name = "Freedom",
+	skingroup = 0,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[11] = {
+	name = "Freedom 2",
+	skingroup = 1,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[12] = {
+	name = "Freedom 3",
+	skingroup = 2,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[13] = {
+	name = "Ecologist",
+	skingroup = 0,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[14] = {
+	name = "Monolith",
+	skingroup = 0,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[15] = {
+	name = "Monolith 2",
+	skingroup = 1,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
+ITEM.skincustom[16] = {
+	name = "Monolith 3",
+	skingroup = 2,
+	modelOverride = "models/stalker_greh/stalker_greh_1a_face_01.mdl",
+}
