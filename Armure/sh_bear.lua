1b18a68608b67be2bd5a82c8b804c1379eb4df3e
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2022-11-28 20:38:53 +0100

    Armure Bear radiation

---
+++ Armure/sh_bear.lua
@@ -0,0 +1,24 @@
+ITEM.name = "Bear Radiation"
+ITEM.model ="models/lostsignalproject/items/repair/armor_repair_pro.mdl"
+ITEM.newModel ="models/stalkertnb/rad_bear.mdl"
+ITEM.description= "Armure Bear Radiation"
+ITEM.longdesc = "L'armure Bear radiation est une armure très recherchée de part son efficacité en combat mais aussi avec les anomalies et les radiations. C'est une armure polivalente."
+ITEM.width = 2
+ITEM.height = 2
+ITEM.price = 100000
+ITEM.busflag = "dev"
+ITEM.br = 2
+ITEM.fbr = 16
+ITEM.ar = 0.60
+ITEM.far = 5
+ITEM.isHelmet = true
+ITEM.isGasmask = true
+ITEM.radProt = 0.9
+ITEM.repairCost = ITEM.price/100*1
+ITEM.ballisticlevels = {"ll","lV","lV","lV","lll-a"}
+ITEM.img = ix.util.GetMaterial("cotz/ui/icons/outfit_126.png")
+ITEM.overlayPath = "vgui/overlays/hud_exo1.png"
+ITEM.noBusiness = true
+
+ITEM.weight = 12.400
+ITEM.miscslots = 4
