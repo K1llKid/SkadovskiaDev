96f31791993d279d4d80be98af14f1ef308af87f
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2022-11-30 20:52:16 +0100

    Exosquelette

---
+++ Armure/sh_dutyexo.lua
@@ -0,0 +1,24 @@
+ITEM.name = "Exosquelette DUTY"
+ITEM.model ="models/kek1ch/exo_outfit.mdl"
+ITEM.newModel ="models/player/stalker_dolg/dolg_exo/dolg_exo.mdl"
+ITEM.description= "Exosquelette de la Faction DUTY"
+ITEM.longdesc = "Un exosquelette entier porté par les duty"
+ITEM.width = 2
+ITEM.height = 2
+ITEM.price = 100000
+ITEM.busflag = "dev"
+ITEM.br = 2
+ITEM.fbr = 16
+ITEM.ar = 0.60
+ITEM.far = 5
+ITEM.isHelmet = true
+ITEM.isGasmask = true
+ITEM.radProt = 0.9
+ITEM.repairCost = ITEM.price/100*1
+ITEM.ballisticlevels = {"ll","lV","lV","lV","lll-a"}
+ITEM.img = ix.util.GetMaterial("cotz/ui/icons/outfit_128.png")
+ITEM.overlayPath = "vgui/overlays/hud_exo1.png"
+ITEM.noBusiness = true
+
+ITEM.weight = 12.400
+ITEM.miscslots = 4
