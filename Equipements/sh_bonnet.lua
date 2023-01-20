107b416762c819f73863992ccbe5f9b51a412311
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2023-01-20 23:57:26 +0100

    Un bonnet ! Oui.

---
+++ Equipements/sh_bonnet.lua
@@ -0,0 +1,79 @@
+ITEM.name = "Bonnet"
+ITEM.description = "Un vieux bonnet"
+ITEM.longdesc = "Ce vieux bonnet est le bonnet parfait pour ne pas avoir froid dans la zone, si vous avez tendances à avoir froid aux oreilles, c'est ce qu'il vous faut !"
+ITEM.model = "models/projectpt/headwear_beanie.mdl"
+ITEM.price = 1000
+--ITEM.busflag = {"ARMOR4", "SPECIAL6_1"}
+ITEM.busflag = {"headgear"}
+ITEM.br = 0.05
+ITEM.fbr = 0
+ITEM.ar = 0
+ITEM.far = 0
+ITEM.radProt = 0
+ITEM.isGasmask = nil
+ITEM.isHelmet = true
+ITEM.ballisticlevels = {"0"}
+ITEM.repairCost = ITEM.price/10*1
+
+ITEM.weight = 100
+
+[1] = {
+	["children"] = {
+		[1] = {
+			["children"] = {
+			},
+			["self"] = {
+				["Skin"] = 0,
+				["UniqueID"] = "cf9fa3042d2d3e2c2b0ffbbdfe16c932187ca2c3d0695df49fe0adc67aea7936",
+				["NoLighting"] = false,
+				["AimPartName"] = "",
+				["IgnoreZ"] = false,
+				["AimPartUID"] = "",
+				["Materials"] = "",
+				["Name"] = "",
+				["LevelOfDetail"] = 0,
+				["NoTextureFiltering"] = false,
+				["PositionOffset"] = Vector(0, 0, 0),
+				["IsDisturbing"] = false,
+				["EyeAngles"] = false,
+				["DrawOrder"] = 0,
+				["TargetEntityUID"] = "",
+				["Alpha"] = 1,
+				["Material"] = "",
+				["Invert"] = false,
+				["ForceObjUrl"] = false,
+				["Bone"] = "head",
+				["Angles"] = Angle(-20.710172653198, -87.631912231445, -72.216209411621),
+				["AngleOffset"] = Angle(0, 0, 0),
+				["BoneMerge"] = false,
+				["Color"] = Vector(1, 1, 1),
+				["Position"] = Vector(-80.386535644531, -13.092529296875, -25.000122070313),
+				["ClassName"] = "model2",
+				["Brightness"] = 1,
+				["Hide"] = false,
+				["NoCulling"] = false,
+				["Scale"] = Vector(1, 1, 1),
+				["LegacyTransform"] = false,
+				["EditorExpand"] = false,
+				["Size"] = 1.3,
+				["ModelModifiers"] = "",
+				["Translucent"] = false,
+				["BlendMode"] = "",
+				["EyeTargetUID"] = "",
+				["Model"] = "models/projectpt/headwear_beanie.mdl",
+			},
+		},
+	},
+	["self"] = {
+		["DrawOrder"] = 0,
+		["UniqueID"] = "d742eecea559e005e039dc44fb767fbd565591d9a8dabe5721369365de8d5981",
+		["Hide"] = false,
+		["TargetEntityUID"] = "",
+		["EditorExpand"] = true,
+		["OwnerName"] = "self",
+		["IsDisturbing"] = false,
+		["Name"] = "my outfit",
+		["Duplicate"] = false,
+		["ClassName"] = "group",
+	},
+},
