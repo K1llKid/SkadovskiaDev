0d67f91ad8cbad839b215dc31a5738d271966627
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2023-01-21 00:36:45 +0100

    Exosquelette Mercenaire Proto I

---
+++ Armure/sh_merc3.lua
@@ -0,0 +1,28 @@
+ITEM.name = "Exosquelette Mercenaire"
+ITEM.model = "models/lostsignalproject/items/upgrades/upgro3.mdl"
+ITEM.newModel = "models/stalker_merc/stalker_merc4a.mdl"
+ITEM.description= "L'Exo des Mercenaires" 
+ITEM.longdesc = "Cette armure est le produit de recherches militaires avancées, initialement conçue pour les militaires ukrainiens afin d'assurer des missions à haut risque dans la Zone. Cependant, à cause de l'énorme coût d'un seul produit ainsi que des nombreux problèmes techniques et défauts de conception, l'Exosquelette n'a jamais atteint le cap de la production de masse, mais continue à être produit en nombre limité dans des usines souterraines avant d'être livrées dans la Zone."
+ITEM.width = 2
+ITEM.height = 3
+ITEM.price = 180000
+ITEM.busflag = "dev"
+ITEM.br = 0.35
+ITEM.fbr = 10
+ITEM.ar = 0.28
+ITEM.far = 3
+ITEM.isHelmet = true
+ITEM.isGasmask = true
+ITEM.radProt = 0.9
+ITEM.repairCost = ITEM.price/100*1
+ITEM.ballisticlevels = {"lll","lV","lV","lll-a","lll-a"}
+ITEM.img = ix.util.GetMaterial("cotz/ui/icons/bronequip4.png")
+ITEM.noBusiness = true
+
+ITEM.weight = 14.500
+ITEM.miscslots = 6
+
+ITEM.newSkin = 0
+ITEM.bodygroup = {0}
+ITEM.bodygroupsub = {0}
+
