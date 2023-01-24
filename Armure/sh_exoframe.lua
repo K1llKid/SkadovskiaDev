686c2acaf5211e9fddc72fb56f35eef791ffe1c5
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2023-01-24 21:34:57 +0100

    Tentative de portabilité de charge

---
+++ Armure/sh_exoframe.lua
@@ -0,0 +1,118 @@
+ITEM.name = "Cadre de l'exosquelette"
+ITEM.model ="models/kek1ch/exolight_outfit.mdl"
+ITEM.replacements ="models/projectpt/suits/male_exoskeleton.mdl"
+ITEM.description= "Un cadre d'exosquelette."
+ITEM.longdesc = "Système d'exosquelette actif complet de dernière génération, de 3ème génération, d'origine inconnue. Les servos limitent la vitesse et la précision des mouvements, mais allègent efficacement la charge de l'utilisateur et multiplient sa force physique par deux."
+ITEM.width = 2
+ITEM.height = 3
+ITEM.price = 150000
+ITEM.busflag = "dev"
+ITEM.br = 0
+ITEM.fbr = 0
+ITEM.ar = 0
+ITEM.far = 0
+ITEM.repairCost = ITEM.price/100*1
+ITEM.ballisticlevels = {"0","0","0","0","0"}
+ITEM.img = ix.util.GetMaterial("vgui/hud/radsuit.png")
+ITEM.noBusiness = true
+
+
+ITEM.newSkin = 0
+ITEM.bodyGroups = {
+	["masks"] = 2,
+}
+
+
+
+ITEM.skincustom[1] = {
+	name = "Skin 0",
+	skingroup = 0,
+}
+ITEM.skincustom[2] = {
+	name = "Skin 1",
+	skingroup = 1,
+}
+ITEM.skincustom[3] = {
+	name = "Skin 2",
+	skingroup = 2,
+}
+ITEM.skincustom[4] = {
+	name = "Skin 3",
+	skingroup = 3,
+}
+ITEM.skincustom[5] = {
+	name = "Skin 4",
+	skingroup = 4,
+}
+ITEM.skincustom[6] = {
+	name = "Skin 5",
+	skingroup = 5,
+}
+ITEM.skincustom[7] = {
+	name = "Skin 6",
+	skingroup = 6,
+}
+ITEM.skincustom[8] = {
+	name = "Skin 7",
+	skingroup = 7,
+}
+ITEM.skincustom[9] = {
+	name = "Skin 8",
+	skingroup = 8,
+}
+ITEM.skincustom[10] = {
+	name = "Skin 9",
+	skingroup = 9,
+}
+ITEM.skincustom[11] = {
+	name = "Skin 10",
+	skingroup = 10,
+}
+ITEM.skincustom[12] = {
+	name = "Skin 11",
+	skingroup = 11,
+}
+ITEM.skincustom[13] = {
+	name = "Skin 12",
+	skingroup = 12,
+}
+ITEM.skincustom[14] = {
+	name = "Skin 13",
+	skingroup = 13,
+}
+ITEM.skincustom[15] = {
+	name = "Skin 14",
+	skingroup = 14,
+}
+ITEM.skincustom[16] = {
+	name = "Skin 15",
+	skingroup = 15,
+}
+ITEM.skincustom[17] = {
+	name = "Skin 16",
+	skingroup = 16,
+}
+ITEM.skincustom[18] = {
+	name = "Skin 17",
+	skingroup = 17,
+}
+ITEM.skincustom[19] = {
+	name = "Skin 18",
+	skingroup = 18,
+}
+ITEM.skincustom[20] = {
+	name = "Skin 19",
+	skingroup = 19,
+}
+ITEM.skincustom[21] = {
+	name = "Skin 20",
+	skingroup = 20,
+}
+ITEM.skincustom[22] = {
+	name = "Skin 21",
+	skingroup = 21,
+}
+ITEM.skincustom[23] = {
+	name = "Skin 22",
+	skingroup = 22,
+}
