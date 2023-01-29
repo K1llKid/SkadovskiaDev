0614afe9f76710e5d77a824c687b7c8fdb0564a8
A3R1S <38764052+Aeris-py@users.noreply.github.com>
2023-01-29 22:14:52 +0100

    Prévention de crash

---
+++ plugin/crashscreen/sh_plugin.lua
@@ -0,0 +1,12 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Menu de crash"
+PLUGIN.description = "Menu de crash"
+PLUGIN.author = ""
+PLUGIN.schema = "Any"
+PLUGIN.license = [[
+cope
+]]
+
+ix.util.Include("cl_plugin.lua")
+
